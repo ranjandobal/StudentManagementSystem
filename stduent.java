894cf154fc4270069d9697a9d9324b2c5cdfb307
Ranjan <ranjandobal@Ranjans-MacBook-Pro.local>
2025-01-13 15:02:57 +0545

    This my last commit

--- stduent.java
+++ stduent.java
@@ -2,4 +2,12 @@ public class stduent {
     public String name; 
     public String age; 
 
+    public String getName(){
+        return name; 
+    }
+    public String getAge(){
+        return age; 
+    }
+
+
 }
